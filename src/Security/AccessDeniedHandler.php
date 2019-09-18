15f35949ce316c2b04f22a5ab671f83ccd5511fc
Loris <devloris@gmail.com>
2019-09-18 10:36:39 +0200

    :sparkles: Auth

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler extends AbstractController implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return $this->render('errors/error403.html.twig', [
+            'msg' => $accessDeniedException->getMessage()
+        ]);
+    }
+}
